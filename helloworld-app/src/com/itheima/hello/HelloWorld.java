59880e512ec7f446e107a3001657e3afb3423955
Junchen He <junchenhe123@gmail.com>
2024-04-05 21:14:15 -0700

    Java start

---
+++ helloworld-app/src/com/itheima/hello/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.itheima.hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ helloworld-app/src/com/itheima/speedKey/SpeedKeyDemo.java
@@ -0,0 +1,11 @@
+package com.itheima.speedKey;
+
+public class SpeedKeyDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
